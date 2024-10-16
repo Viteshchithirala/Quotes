a8b75ed8ec54a692e85086b42c7bcefff17d2530
Viteshchithirala <viteshkumarchithirala@gmail.com>
2024-10-16 13:09:36 +0530

    I made these changes

---
+++ script.js
@@ -0,0 +1,45 @@
+const quoteConatiner = document.getElementById('quote-container');
+const quoteText = document.getElementById('quote');
+const authorText = document.getElementById('author');
+const newQuoteBtn = document.getElementById('new-quote');
+const loader = document.getElementById('loader');
+
+let apiQuotes =[];
+
+function loading(){
+    loader.hidden=false;
+    quoteConatiner.hidden=true;
+}
+function complete(){
+    quoteConatiner.hidden=false;
+    loader.hidden=true;
+}
+function newQuote(){
+    loading();
+    const quote = apiQuotes[Math.floor(Math.random()*apiQuotes.length)];
+    if(!quote.author){
+        authorText.textContent = 'Unknown';
+    } else{
+        authorText.textContent = quote.author;
+    }
+    if (quote.text.length > 120) {
+        quoteText.classList.add('long-quote');
+       } else {
+        quoteText.classList.remove('long-quote');
+       }
+       quoteText.textContent = quote.text;
+       complete();
+    }
+    async function getQuotes() {
+        loading();
+        const apiUrl = 'https://jacintodesign.github.io/quotes-api/data/quotes.json';
+        try {
+          const response = await fetch(apiUrl);
+          apiQuotes = await response.json();
+          newQuote();
+        } catch (error) {
+            // Catch Error Here
+        }
+}
+newQuoteBtn.addEventListener('click', newQuote);
+getQuotes();
